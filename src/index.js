059885416ff0aeb0cd5e8c195b1c4067b45ed53a
k3ep3r <keeper3211@gmail.com>
2018-02-23 11:06:49 +0300

    My atom make me mad, I dont know where but he always modifi a file (package-lock.json)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var result = {};
+    var array = ['H', 'Q', 'D', 'N', 'P'];
+    var coins = {H : 50, Q : 25, D : 10, N : 5, P : 1};
+
+    if (currency >= 10001) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if (currency <= 0) {
+        return result;
+    }
+
+    for (var i = 0; i < array.length; i++) {
+        result[array[i]] = Math.trunc(currency/coins[array[i]]);
+        if (result[array[i]] <= 0){
+            delete result[array[i]];
+        }
+        currency = currency % coins[array[i]];
+    }
+    return result;
 }
